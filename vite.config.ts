de24f5579fbb78068b54a218ffc8e5c47bba39dc
Abs-80 <120689900+Abs-80@users.noreply.github.com>
2026-06-10 19:06:53 -0400

    Updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/John-Oquendo/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
